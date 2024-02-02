7a3a34db5f93a658020c628342629ab8d6860bc8
sa556693828 <sa556693828@gmail.com>
2024-02-02 16:59:33 +0800

    fix build bug

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,16 +4,12 @@ import yt from "@/assets/ICONS/footer_yt.svg";
 import x from "@/assets/ICONS/footer_x.svg";
 import ig from "@/assets/ICONS/footer_ig.svg";
 import Image from "next/image";
-import { footerPath } from "@/constants/pathList";
-import { BacoContext } from "../BacoProvider";
-import { ytLink, igLink, xLink } from "@/constants/socialLink";
 
 interface Props {
   urlPath: string;
 }
 
 export default function Footer({ urlPath }: Props) {
-  const { goPage } = useContext(BacoContext);
   const openLink = (link: string) => {
     window.open(link, "_blank");
   };
@@ -21,31 +17,16 @@ export default function Footer({ urlPath }: Props) {
     <footer className="flex w-full flex-col gap-16 border-t border-bakoB/20 bg-bakoW px-14 py-12 transition-all lg:py-16">
       <div className="flex flex-col gap-8 lg:flex-row lg:justify-between">
         <div className="flex flex-col gap-6 text-sm">
-          {footerPath.map((path) => (
+          {/* {footerPath.map((path) => (
             <a key={path} aria-label={path}>
               {path}
             </a>
-          ))}
+          ))} */}
         </div>
         <div className="flex items-start gap-6">
-          <Image
-            src={ig}
-            alt="Instagram"
-            className="h-6 w-6 lg:w-full"
-            onClick={() => openLink(igLink)}
-          />
-          <Image
-            src={yt}
-            alt="Instagram"
-            className="h-6 w-6 lg:w-full"
-            onClick={() => openLink(ytLink)}
-          />
-          <Image
-            src={x}
-            alt="Instagram"
-            className="h-6 w-6 lg:w-full"
-            onClick={() => openLink(xLink)}
-          />
+          <Image src={ig} alt="Instagram" className="h-6 w-6 lg:w-full" />
+          <Image src={yt} alt="Instagram" className="h-6 w-6 lg:w-full" />
+          <Image src={x} alt="Instagram" className="h-6 w-6 lg:w-full" />
         </div>
       </div>
       <div className="flex text-[10px] leading-[120%] opacity-70">
